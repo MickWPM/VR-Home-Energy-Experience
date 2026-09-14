f0d2a0c5448a037c2583da33a563fca76da43dfe
Mick M <mickwpmcdonnell@gmail.com>
2026-09-14 17:16:16 +0930

    Added FPS to dev only and updated lighting

---
+++ Assets/Scripts/DevelopmentOnly/FPSDebug.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+
+public class FPSDebug: MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI fpsText;
+    [SerializeField] private float updateInterval = 0.5f;
+
+    private float accumulatedTime = 0f;
+    private int frameCount = 0;
+
+    void Update()
+    {
+        accumulatedTime += Time.unscaledDeltaTime;
+        frameCount++;
+
+        if (accumulatedTime >= updateInterval)
+        {
+            float fps = frameCount / accumulatedTime;
+            fpsText.text = $"{Mathf.RoundToInt(fps)} FPS";
+
+            accumulatedTime = 0f;
+            frameCount = 0;
+        }
+    }
+}
