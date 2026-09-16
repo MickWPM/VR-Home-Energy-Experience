c9c66f00991c2d741104d4b4ee38e66167e6729a
Mick M <mickwpmcdonnell@gmail.com>
2026-09-17 09:29:39 +0930

    Initial tutorial/state work

---
+++ Assets/Scripts/StateManager/IState.cs
@@ -0,0 +1,11 @@
+//Extend the fish state machine to be a generic for "context type"
+//This means we dont have to create custom methods with arbitrary inputs
+//Slight overhead by creating machine specific context but means it is fully reusable
+public interface IState<TContext>
+{
+    string StateName { get; }
+
+    void EnterState(TContext context);
+    void UpdateState(TContext context, float deltaTime);
+    void ExitState(TContext context);
+}
